b52cec8ed2729b20f3d86413ba78bab93553bdd0
Denis <denismagin@gmail.com>
2022-07-21 10:01:52 +0300

    added a timer and starting flag placement om right mouse click

--- js/main.js
+++ js/main.js
@@ -4,12 +4,19 @@ const gLevel = { SIZE: 4, MINES: 2 }
 const gGame = { isOn: false, shownCount: 0, secsPassed: 0 }
 
 const BOMB = '&#128163'
+const FLAG = '&#128681'
 let gBoard
 
 const elTable = document.getElementById('table')
+let elTimer = document.getElementById('timer')
+
 
 function init() {
     console.log('hello')
+    elTable.addEventListener('contextmenu', e => {
+        onRightMouseClick(e)
+        e.preventDefault()
+      })
     startGame() // for debuging
 }
 
@@ -19,6 +26,7 @@ function startGame() {
     gGame.isOn = true
     gBoard = createBoard()
     renderBoard(gBoard)
+    scatterMines(gBoard)
     setMinesNegsCount(gBoard)
     //scatterMines(gBoard)
 }
@@ -37,6 +45,9 @@ function setMinesNegsCount(board) {
 function cellClicked(elCell, i, j) {
     // console.log(gBoard)
     // console.log('clicked: ', elCell, i, j)
+    
+    const isFirstClick = gGame.secsPassed === 0 ? setInterval(function () { countSeconds() }, 1000) : false
+    
     const currCell = gBoard[i][j]
 
     if (currCell.isShown) {
@@ -50,6 +61,23 @@ function cellClicked(elCell, i, j) {
         currCell.isShown = true
         elCell.style.color = 'black'
     }
+}
 
+
+function scatterMines(board) {
+
+    board[getRandomInt(0,4)][getRandomInt(0,4)].isMine = true
+    board[getRandomInt(0,4)][getRandomInt(0,4)].isMine = true
+    renderBoard(board)
+}
+
+
+function countSeconds() {
+    gGame.secsPassed++
+    elTimer.innerHTML = gGame.secsPassed
 
 }
+
+
+
+  
